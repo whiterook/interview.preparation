5c310983b4dc74d0786463559dc53022d7819279
splotnikov <splotnikov@gmail.com>
2014-07-30 23:56:18 -0400

    Initial commit

---
+++ src/com/splotnikov/algorithms/StringAlgorithms.java
@@ -0,0 +1,28 @@
+package com.splotnikov.algorithms;
+
+import java.security.InvalidParameterException;
+
+public class StringAlgorithms {
+
+	public int findMatch(String pattern, String text) 
+			throws InvalidParameterException {
+				
+		int patternLength = pattern.length();
+		int textLength = text.length();
+		
+		if(textLength < patternLength)
+			throw new InvalidParameterException();
+		
+		
+		for (int i = 0; i <= (textLength - patternLength); i++) {
+			int j = 0;
+			while (j < patternLength && pattern.charAt(j) == text.charAt(i + j)) {				
+				j++;
+			}
+			if(j == patternLength)
+				return i;
+		}
+		
+		return -1;
+		}
+}
