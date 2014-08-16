cf27b9b306b74bfa2bfa6867bb48e62e963b3bb6
sergeypl <splotnikov@gmail.com>
2014-08-16 15:13:12 -0400

    eclipse auto change

---
+++ src/com/splotnikov/algorithms/StringAlgorithmsTest.java
@@ -0,0 +1,11 @@
+package com.splotnikov.algorithms;
+
+import junit.framework.TestCase;
+
+public class StringAlgorithmsTest extends TestCase {
+
+	public StringAlgorithmsTest(String name) {
+		super(name);
+	}
+
+}
