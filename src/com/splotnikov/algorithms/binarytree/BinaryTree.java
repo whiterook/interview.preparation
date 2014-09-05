a6e0e163bcf11b86df6b39ccb296106df83812fc
splotnikov <splotnikov@gmail.com>
2014-09-04 21:03:57 -0400

    added inOrderTreverseTree

--- src/com/splotnikov/algorithms/binarytree/BinaryTree.java
+++ src/com/splotnikov/algorithms/binarytree/BinaryTree.java
@@ -4,8 +4,63 @@ public class BinaryTree {
 
 	private Node root;
 	
+	public void setRoot(Node root){
+		this.root = root;
+	}
+	
+	public Node getRoot(){
+		return this.root;
+	}
+	
+	
 	public void addNode(int key, String value){
 		
 		Node newNode = new Node(key, value);
+		Node parentNode = findParent(key);
+		if(parentNode == null){
+			setRoot(newNode);
+		}
+		else{
+			if(key < parentNode.getKey()){
+				parentNode.setLeftChild(newNode);
+			}
+			else{
+				parentNode.setRightChild(newNode);
+			}
+		}
+	}
+	
+	private Node findParent(int key){
+		if(getRoot() == null)
+			return null;
+		
+		Node currentNode = getRoot();
+		
+		while(true){
+			if(key < currentNode.getKey()){
+				if(currentNode.getLeftChild() == null){
+					return currentNode;
+				}
+				else{
+					currentNode = currentNode.getLeftChild();
+				}
+			}
+			else{
+				if(currentNode.getRightChild() == null){
+					return currentNode;
+				}
+				else{
+					currentNode = currentNode.getRightChild();
+				}
+			}
+		}
+	}
+	
+	public void inOrderTreverseTree(Node node){
+		if(node != null){
+			inOrderTreverseTree(node.getLeftChild());
+			System.out.println(node);
+			inOrderTreverseTree(node.getRightChild());
+		}
 	}
 }
--- src/com/splotnikov/algorithms/tests/BinaryTreeTests.java
+++ src/com/splotnikov/algorithms/tests/BinaryTreeTests.java
@@ -1,5 +1,21 @@
 package com.splotnikov.algorithms.tests;
 
+import org.junit.Test;
+
+import com.splotnikov.algorithms.binarytree.BinaryTree;
+
 public class BinaryTreeTests {
 
+	@Test
+	public void inOrderTreverseTests(){
+		BinaryTree theTree = new BinaryTree();
+		theTree.addNode(50, "Boss");
+		theTree.addNode(25, "Vice President");
+		theTree.addNode(15, "Office Manager");
+		theTree.addNode(30, "Secretary");
+		theTree.addNode(75, "Sales Manager");
+		theTree.addNode(85, "Salesman 1");		
+		
+		theTree.inOrderTreverseTree(theTree.getRoot());
+	}
 }
